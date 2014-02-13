cfadc869f08bd7cc40da933a69d5fbe7ff6ba658
Willy Weiskopf <william@weiskopf.me>
2014-02-12 22:04:28 -0700

    The start of the static page.

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quick-Fill Sandbagger</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1 class="text-center">Quick-Fill Sandbagger</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <p class="lead text-center">We have a superb product that we're eager to share with you.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-6 col-xs-offset-3">
+          <p>We're still rolling out our full website.  In the meantime, please contact us directly.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-3 col-xs-offset-2 panel">
+          <address>
+            <h2>Greg</h2>
+            <a href="mailto:greg@indmfg.net">greg@indmfg.net</a>
+          </address>
+        </div>
+        <div class="col-xs-3 col-xs-offset-2 panel">
+          <address>
+            <h2>Jerry</h2>
+            <a href="mailto:jerry@indmfg.net">jerry@indmfg.net</a>
+          </address>
+        </div>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
