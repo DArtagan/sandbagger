c27bcab26aa41767405c75a5acf51a9e151e17e7
Willy Weiskopf <william@weiskopf.me>
2014-02-19 01:47:31 -0700

    Primary navbar, affixed it, fixed carousel.

--- index.php
+++ index.php
@@ -18,25 +18,70 @@
   </head>
   <body>
     <div id="wrap" class="container-fluid">
-      <div id="carousel-head" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-head" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-head" data-slide-to="1"></li>
-          <li data-target="#carousel-head" data-slide-to="2"></li>
-        </ol>
+      <div class="row">
+        <div class="col-xs-12">
+          <div id="carousel-head" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-head" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-head" data-slide-to="1"></li>
+              <li data-target="#carousel-head" data-slide-to="2"></li>
+            </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-          <div class="item active">
-            <img src="http://placekitten.com/2000/600" alt="Kitten 1">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="http://placekitten.com/2000/400" alt="Kitten 1">
+              </div>
+              <div class="item">
+                <img src="http://placekitten.com/g/2000/400" alt="Kitten 2"> 
+              </div>
+              <div class="item">
+                <img src="http://placekitten.com/2002/400" alt="Kitten 3"> 
+              </div>
+            </div>
           </div>
-          <div class="item">
-            <img src="http://placekitten.com/g/2000/600" alt="Kitten 2"> 
-          </div>
-          <div class="item">
-            <img src="http://placekitten.com/2002/600" alt="Kitten 3"> 
-          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <header id="nav-primary" class="navbar navbar-inverse" data-spy="affix" data-offset-top="350">
+            <div class="container">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Quick-Fill Sandbagger</a>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#">Story</a></li>
+                  <li><a href="#">Options</a></li>
+                  <li><a href="#">Purchase</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div><!-- /.navbar-collapse -->
+            </div>
+          </header>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem facilisis erat mattis tempor. Nulla cursus interdum tellus, at adipiscing lorem consectetur ac. Pellentesque dignissim nec ligula ut tincidunt. Nunc sollicitudin orci interdum imperdiet dictum. Mauris imperdiet faucibus nibh. Sed pretium metus ut orci ultrices malesuada. Nunc vel felis eu neque tempus dictum. Duis eu nibh interdum felis euismod ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+          <p>In hac habitasse platea dictumst. Praesent rhoncus rutrum augue, quis tristique sem sodales in. Nam tristique at felis eget lobortis. Sed tristique accumsan euismod. Aenean a lacinia lorem. Morbi venenatis nunc pretium arcu fermentum, non pretium leo congue. Nunc commodo, ante porttitor scelerisque lacinia, lectus lorem congue turpis, et ultrices eros diam nec arcu. Donec a placerat velit. Aenean eu purus hendrerit, facilisis ipsum id, mollis dui. Donec gravida, dolor vel congue ultrices, justo sapien luctus tortor, eu feugiat risus purus sit amet magna. Proin tincidunt auctor sapien, quis pellentesque justo congue ut.</p>
+          <p>Nam hendrerit tincidunt lectus, ac lacinia urna tristique ornare. Proin dictum massa sed eros volutpat laoreet. Sed fermentum lobortis nunc non porta. Sed vitae mi et nisi rutrum vestibulum quis non lacus. Sed scelerisque, mi non ullamcorper faucibus, ante purus suscipit lectus, at iaculis nibh nisl vel velit. Ut egestas mi felis. Sed pharetra malesuada lacus, nec auctor risus cursus a. Integer fringilla nunc at consequat luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras condimentum tellus ante, vitae lacinia nunc tristique et. Aliquam at gravida nunc. Nulla quis odio auctor, dignissim massa sed, porttitor dolor. Sed luctus lectus nec tortor ullamcorper tempus.</p>
+          <p>Maecenas et sapien augue. Praesent hendrerit venenatis felis, vitae varius enim fermentum nec. Suspendisse vel dui porta, adipiscing lorem pellentesque, porta orci. Proin vestibulum augue lacus, vel vulputate mi aliquet accumsan. Donec vel sem diam. Cras enim quam, rhoncus et sagittis id, interdum non diam. Etiam a tellus vel dolor laoreet rhoncus. Sed facilisis tincidunt erat, mollis placerat orci auctor eu. Nunc et mauris et risus lobortis commodo. Sed nisi risus, pretium ac imperdiet id, convallis et odio. Praesent eleifend velit lacinia eros blandit, et volutpat metus faucibus. Pellentesque erat risus, condimentum vitae condimentum ac, laoreet quis dui. Nunc sit amet turpis sit amet nisl mollis malesuada ac et enim. Proin non massa tempus risus egestas lacinia eu et augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam elementum ultricies nisi id porta.</p>
+          <p>Suspendisse eu facilisis ipsum, auctor adipiscing sem. Mauris tempus elit erat, interdum pretium tellus dictum a. Curabitur luctus metus non vehicula sodales. Nam vel nisl eget urna venenatis dignissim. Fusce auctor tempus mi, vel sollicitudin ligula feugiat non. Maecenas condimentum dui velit, in venenatis lorem varius ut. Maecenas et massa tempor nibh volutpat sodales. Quisque sit amet justo sed felis consectetur facilisis. Suspendisse at orci nec dui blandit placerat sodales sed metus. Proin sit amet dictum quam, sit amet elementum nisl. Cras vestibulum dolor eu vulputate luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam et lobortis leo, eu tincidunt elit.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem facilisis erat mattis tempor. Nulla cursus interdum tellus, at adipiscing lorem consectetur ac. Pellentesque dignissim nec ligula ut tincidunt. Nunc sollicitudin orci interdum imperdiet dictum. Mauris imperdiet faucibus nibh. Sed pretium metus ut orci ultrices malesuada. Nunc vel felis eu neque tempus dictum. Duis eu nibh interdum felis euismod ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+          <p>In hac habitasse platea dictumst. Praesent rhoncus rutrum augue, quis tristique sem sodales in. Nam tristique at felis eget lobortis. Sed tristique accumsan euismod. Aenean a lacinia lorem. Morbi venenatis nunc pretium arcu fermentum, non pretium leo congue. Nunc commodo, ante porttitor scelerisque lacinia, lectus lorem congue turpis, et ultrices eros diam nec arcu. Donec a placerat velit. Aenean eu purus hendrerit, facilisis ipsum id, mollis dui. Donec gravida, dolor vel congue ultrices, justo sapien luctus tortor, eu feugiat risus purus sit amet magna. Proin tincidunt auctor sapien, quis pellentesque justo congue ut.</p>
+          <p>Nam hendrerit tincidunt lectus, ac lacinia urna tristique ornare. Proin dictum massa sed eros volutpat laoreet. Sed fermentum lobortis nunc non porta. Sed vitae mi et nisi rutrum vestibulum quis non lacus. Sed scelerisque, mi non ullamcorper faucibus, ante purus suscipit lectus, at iaculis nibh nisl vel velit. Ut egestas mi felis. Sed pharetra malesuada lacus, nec auctor risus cursus a. Integer fringilla nunc at consequat luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras condimentum tellus ante, vitae lacinia nunc tristique et. Aliquam at gravida nunc. Nulla quis odio auctor, dignissim massa sed, porttitor dolor. Sed luctus lectus nec tortor ullamcorper tempus.</p>
+          <p>Maecenas et sapien augue. Praesent hendrerit venenatis felis, vitae varius enim fermentum nec. Suspendisse vel dui porta, adipiscing lorem pellentesque, porta orci. Proin vestibulum augue lacus, vel vulputate mi aliquet accumsan. Donec vel sem diam. Cras enim quam, rhoncus et sagittis id, interdum non diam. Etiam a tellus vel dolor laoreet rhoncus. Sed facilisis tincidunt erat, mollis placerat orci auctor eu. Nunc et mauris et risus lobortis commodo. Sed nisi risus, pretium ac imperdiet id, convallis et odio. Praesent eleifend velit lacinia eros blandit, et volutpat metus faucibus. Pellentesque erat risus, condimentum vitae condimentum ac, laoreet quis dui. Nunc sit amet turpis sit amet nisl mollis malesuada ac et enim. Proin non massa tempus risus egestas lacinia eu et augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam elementum ultricies nisi id porta.</p>
+          <p>Suspendisse eu facilisis ipsum, auctor adipiscing sem. Mauris tempus elit erat, interdum pretium tellus dictum a. Curabitur luctus metus non vehicula sodales. Nam vel nisl eget urna venenatis dignissim. Fusce auctor tempus mi, vel sollicitudin ligula feugiat non. Maecenas condimentum dui velit, in venenatis lorem varius ut. Maecenas et massa tempor nibh volutpat sodales. Quisque sit amet justo sed felis consectetur facilisis. Suspendisse at orci nec dui blandit placerat sodales sed metus. Proin sit amet dictum quam, sit amet elementum nisl. Cras vestibulum dolor eu vulputate luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam et lobortis leo, eu tincidunt elit.</p>
         </div>
       </div>
     </div>
