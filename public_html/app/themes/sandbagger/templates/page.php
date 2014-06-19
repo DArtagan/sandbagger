873635d7fee305b5c6f1a0b7fefa3532718acac4
Willy Weiskopf <william@weiskopf.me>
2014-06-19 12:21:29 -0600

    Page concatenation dark/light styles.

--- public_html/app/themes/sandbagger/templates/page.php
+++ public_html/app/themes/sandbagger/templates/page.php
@@ -1,5 +1,14 @@
-<?php while (have_posts()) : the_post(); ?>
-  <div class="row">
+<?php
+$post_meta = get_post_meta( get_the_id(), 'include_pages', false);
+$group_data = array();
+if ($post_meta) {
+  foreach ($post_meta as $data) {
+    $group_data[$data['page']] = $data['style'];
+  }
+}
+
+while (have_posts()) : the_post(); ?>
+  <div class="row <?php if ($group_data[$post->ID]) { echo $group_data[$post->ID]; }; ?>">
     <div class="section">
       <div class="page-header">
         <h1>
@@ -7,6 +16,7 @@
         </h1>
       </div>
       <?php the_content(); ?>
+      <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     </div>
   </div>
 <?php endwhile; ?>
