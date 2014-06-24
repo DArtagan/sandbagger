6674c7560d4fe165afbcefa9f85dbf9a460bf893
Willy Weiskopf <william@weiskopf.me>
2014-06-23 20:36:58 -0600

    Corrected font-path variable reference for glyphicons.

--- public_html/app/themes/sandbagger/lib/scripts.php
+++ public_html/app/themes/sandbagger/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c70edadea7a7a6fdef472793cfa9569e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9b7e22d5ffd7bc32482d47de78da6b4e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
