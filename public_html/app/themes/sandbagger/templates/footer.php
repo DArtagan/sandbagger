b5ee8dfa72d0d8a2f48d00c4eb260edf6e259c99
Willy Weiskopf <william@weiskopf.me>
2014-06-25 13:45:24 -0600

    Strange bug catching firebug.  Changed footer class to fix.

--- public_html/app/themes/sandbagger/templates/footer.php
+++ public_html/app/themes/sandbagger/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
+    <div class="col-xs-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     </div>
