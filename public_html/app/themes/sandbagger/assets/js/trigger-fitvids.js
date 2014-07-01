7820bc806bb5f78e9d57b8f261916f261a982db9
Willy Weiskopf <william@weiskopf.me>
2014-07-01 11:55:32 -0600

    Fitvids trigger for Google drive video, given embed-gdrive-video class.

--- public_html/app/themes/sandbagger/assets/js/trigger-fitvids.js
+++ public_html/app/themes/sandbagger/assets/js/trigger-fitvids.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
     $(".embed-youtube").fitVids();
+    $(".embed-gdrive-video").fitVids({ customSelector: "iframe[src^='https://docs.google.com'] "});
 });
